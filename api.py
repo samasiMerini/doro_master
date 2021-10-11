a9f42e02cce6ccf8fcabbefd7d666f764c74fd82
Abdessamad Merini <abdessamad@MacBook-Pro-de-Abdessamad.local>
2021-10-12 00:16:37 +0100

    update api

--- api.py
+++ api.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 Pkey = st.secrets["Pkey"]              #API KEYS 
 Skey = st.secrets["Skey"]              #API KEY 
-APKI_TELEGRAM = st.secrets["Skey"] 
+APKI_TELEGRAM = st.secrets["APKI_TELEGRAM"] 
 
 CHAT_ID = "-1001577752802"
