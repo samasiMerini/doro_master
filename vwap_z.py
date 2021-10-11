755596975572b5893678b3bebeb5fdf90b3f26c9
Abdessamad Merini <abdessamad@MacBook-Pro-de-Abdessamad.local>
2021-10-12 00:34:21 +0100

    update api

--- vwap_z.py
+++ vwap_z.py
@@ -57,7 +57,7 @@ def touchGreenLine(pds,df,ticker):
     if "tracking" not in  message: 
         print(notifications.sendMessage(message=message))
     else:
-        print(notifications.sendMessage(message=message))
+        print(message)
         
 while True:
     print("Time: %s" % time.ctime())
